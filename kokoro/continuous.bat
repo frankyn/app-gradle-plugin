01dd0508a6b68fd4fde3bae86051d90688c2c296
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-09 18:12:25 -0400

    debugging what's wrong with formatting on Win - fifth attempt

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -6,7 +6,8 @@ call gcloud.cmd components update --quiet
 call gcloud.cmd components install app-engine-java --quiet
 
 call gradlew.bat googleJavaFormat
-git diff
+git config color.diff.whitespace "red reverse"
+git --color -R diff
 REM call gradlew.bat check
 REM curl -s https://codecov.io/bash | bash
 
