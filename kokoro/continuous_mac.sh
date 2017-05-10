fa1f0e64424977a6cf7c0c2f9a4ae5eaa7135f44
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-10 12:21:34 -0400

    add macos build

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -5,8 +5,13 @@ set -e
 # Display commands to stderr.
 set -x
 
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-154.0.1-darwin-x86_64.tar.gz
+tar -xzvf google-cloud-sdk-154.0.1-darwin-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+
+./google-cloud-sdk/bin/gcloud components update
+./google-cloud-sdk/bin/gcloud components install app-engine-java
+
 cd github/app-gradle-plugin
-gcloud components update
-gcloud components install app-engine-java
 ./gradlew check
 # bash <(curl -s https://codecov.io/bash)
