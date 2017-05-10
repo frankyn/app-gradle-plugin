638f4be6c7bc4359f1d23568a75b854773eb08d4
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-05-10 11:24:57 -0400

    debugging what's wrong with formatting on Win - n+1 attempt

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -2,13 +2,13 @@
 
 cd github/app-gradle-plugin
 
-rem call gcloud.cmd components update --quiet
-rem call gcloud.cmd components install app-engine-java --quiet
+call gcloud.cmd components update --quiet
+call gcloud.cmd components install app-engine-java --quiet
 
-call gradlew.bat googleJavaFormat
-git config color.diff.whitespace "red reverse"
-git diff --color --word-diff-regex=.
-REM call gradlew.bat check
+rem call gradlew.bat googleJavaFormat
+rem git config color.diff.whitespace "red reverse"
+rem git diff --color --word-diff-regex=.
+call gradlew.bat check
 REM curl -s https://codecov.io/bash | bash
 
 exit /b %ERRORLEVEL%
